cfe9eb79b46985c395b1b0b139afbd49c8ce386d
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2023-12-20 19:21:30 +0600

    added design

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,43 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-import Todo from './components/Todo'
-import Todolist from './components/Todolist';
+
+import Todolist from './components/Todolist'
+
 
 function App() {
 
-  const[listTodo,setListTodo]=useState([]);
-
-
-  const listAdd=(input)=>{
-    if (input!==""){
-      setListTodo([...listTodo,input])
-
-    }
-else{
-      console.log("Enter a value")
-    }
-
-  }
-
-  const deleteItem=(key)=>{
-
-      let newList=[...listTodo];
-      newList.splice(key,1)
-      setListTodo([...newList]);
-  }
+ 
 
   return (
     <>
-    <h1 className='text-center text-success pt-3 display-3'>To-do app</h1>
-    <Todo addList={listAdd}/>
-    <h5 className='display-5 text-success text-center pt-5'>Following are the added task of yours</h5>
-    <hr></hr>
-    
-          {listTodo.map((item, i) => {
-        return <Todolist index={i} key={i} item={item} delete={deleteItem} />;
-      })}
-  
-
+    <Todolist></Todolist>
     </>
   )
 }
--- to-do-app/src/components/Todolist.jsx
+++ to-do-app/src/components/Todolist.jsx
@@ -1,19 +1,81 @@
+import { useState } from "react";
+import "../css/style.css"
 
-const Todolist = (props) => {
-    return ( 
-        <div className="list-div">
-        <div className="list-container p-1">
-        <li className=" list border border-1 border-dark p-4 m-3">
-            {props.item}
-            <span className="icons p-4">
-            <i className="fa-solid fa-trash fa-lg icon-delete" onClick={e=>{
-                props.delete(props.index);
-            }}></i>
-            </span>
-        </li>
+const Todolist = () => {
+
+        const [tasks,setTasks]=useState([]); //about task list 
+        const [newTask,setNewtask]=useState("") //about the task input
+
+
+        const handleInputChange=(e)=>{
+
+            setNewtask(e.target.value)
+
+        }
+
+        const addTask=()=>{
+
+            if(newTask.trim()!==""){
+                setTasks(t=>[...t,newTask])
+                setNewtask("")
+
+            }
+            
+        }
+        const deleteTask=(i)=>{
+
+            const updatedTasks=tasks.filter((_,index)=>index!==i);
+            setTasks(updatedTasks);
+
+
+        }
+        const moveTaskup = (index) => {
+            if (index > 0) {
+                const updatedTasks = [...tasks];
+                [updatedTasks[index], updatedTasks[index - 1]] =
+                    [updatedTasks[index - 1], updatedTasks[index]];
+                setTasks(updatedTasks);
+            }
+        }
+        const moveTaskDown=(index)=>{
+
+            if(index < tasks.length - 1){
+                const updatedTasks = [...tasks];
+                [updatedTasks[index], updatedTasks[index + 1]] = 
+                [updatedTasks[index + 1], updatedTasks[index]];
+                setTasks(updatedTasks);
+            }
+        }
+
+
+
+
+
+
+
+    
+    return (
+
+        <div className="to-do-list">
+            <h1 className="text-center text-success display-3">To do app</h1>
+            <hr></hr>
+            <label className="label me-4">Task input-</label>
+            <input type="text" placeholder="Input your task..." value={newTask} onChange={handleInputChange} className="p-3 pt-4 me-3 mb-4"/>
+            <button className="btn btn-success p-3 mt-0 mb-2" onClick={addTask}>Add (+)</button>
+            <ol9uoli8>
+                                {tasks.map((e, i) => (
+                        <li key={i}>
+                            <span>{e}</span>
+                            <button className="btn btn-danger ms-4 px-4 py-2 me-3" onClick={()=>deleteTask(i)}> Delete </button>
+                            <button className="btn btn-primary px-4 py-2 me-3" onClick={()=>moveTaskup(i)}> Move up </button>
+                            <button className="btn btn-primary px-4 py-2 me-3" onClick={()=>moveTaskDown(i)}> Move Down </button>
+                        </li>
+                    ))}
+
+            </ol9uoli8>
         </div>
-        </div>
-     );
+
+      );
 }
  
 export default Todolist;
